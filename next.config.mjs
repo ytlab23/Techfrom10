9a675a56987949c75aee1c1fe8ba601ec700e6ed
Shadowctrl <shadowctrl@outlook.com>
2024-08-08 12:17:06 +0530

    Links SRC

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "www.researchgate.net",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
