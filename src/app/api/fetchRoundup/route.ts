69298e9360eda78c0a7bdff062258bd528859438
Shadowctrl <shadowctrl@outlook.com>
2024-08-10 11:31:55 +0530

    Fetch ID/POST

--- src/app/api/fetchRoundup/route.ts
+++ src/app/api/fetchRoundup/route.ts
@@ -1,11 +1,28 @@
 import clientPromise from "@/lib/db";
-export const GET = async () => {
+
+const getDB = async () => {
   const client = await clientPromise;
   const db = client.db(process.env.mongo_db_name);
+
+  return db;
+};
+
+export const GET = async () => {
+  const db = await getDB();
   const collections = db.collection(`${process.env.mongo_collec}`);
   const docs = await collections.find().toArray();
 
   return new Response(JSON.stringify(docs), {
+    headers: { "Content-Type": "application/json" },
+  });
+};
+
+export const POST = async (req: Request) => {
+  const db = await getDB();
+  const { id } = await req.json();
+  const collections = db.collection(`${process.env.mongo_collec}`);
+  const doc = await collections.findOne({ _id: id });
+  return new Response(JSON.stringify(doc), {
     headers: { "Content-Type": "application/json" },
   });
 };
