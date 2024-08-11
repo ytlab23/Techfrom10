1c2d0849a509d18a0bf68e3970a606c19e399dd6
Shadowctrl <shadowctrl@outlook.com>
2024-08-11 16:12:43 +0530

    Hashtags

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+"use client";
 import { NextPage } from "next";
 import Link from "next/link";
 import Image from "next/image";
 import { Badge } from "@/components/ui/badge";
+import { useEffect, useState } from "react";
+
 interface Props {}
+
 interface dataprop {
   _id: string;
   title: string;
@@ -12,19 +16,54 @@ interface dataprop {
   published: string[];
   hashtags: string[];
 }
-const Page: NextPage<Props> = async ({}) => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_BASE_URL}` + "/api/fetchRoundup",
-    { cache: "no-cache" }
-  );
-  const data: dataprop[] = await res.json();
+
+const Page: NextPage<Props> = ({}) => {
+  const [data, setData] = useState<dataprop[]>([]);
+  const [tag, setTag] = useState<string | null>(null);
+  const [filteredData, setFilteredData] = useState<dataprop[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/fetchRoundup`,
+        { cache: "no-cache" }
+      );
+      const data: dataprop[] = await res.json();
+      setData(data);
+      setTag("Uncategorized");
+      setFilteredData(data);
+    };
+    fetchData();
+  }, []);
+
+  const handleTag = (hashtag: string) => {
+    setTag(hashtag);
+  };
+
+  useEffect(() => {
+    if (tag) {
+      if (tag == "Uncategorized") return setFilteredData(data);
+      const newFilteredData = data
+        .map((item) => ({
+          ...item,
+          headlines: item.headlines.filter(
+            (_, idx) => item.hashtags[idx] === tag
+          ),
+        }))
+        .filter((item) => item.headlines.length > 0);
+
+      setFilteredData(newFilteredData);
+    } else {
+      setFilteredData(data);
+    }
+  }, [tag, data]);
+
   return (
     <div className="hero-parent">
       <div className="hero-container-wrap">
-        {data.map((val, index) => (
-          <div key={index + 1} className="hero-container">
+        {filteredData.map((val) => (
+          <div key={val._id} className="hero-container">
             <div className="hero-container-head">
-              {" "}
               <Link href={"/view/" + val._id.toString()}>
                 <h2>{val.title}</h2>
               </Link>
@@ -34,18 +73,18 @@ const Page: NextPage<Props> = async ({}) => {
                 height={1024}
                 alt={val.title}
               />
-            </div>{" "}
+            </div>
             <ul>
               {val.headlines.map((h, hindex) => (
-                <li key={hindex + 2}>{h}</li>
+                <li key={hindex}>{h}</li>
               ))}
-            </ul>{" "}
+            </ul>
             <Link href={"/view/" + val._id.toString()} className="view-in-full">
               View in Full
             </Link>
           </div>
         ))}
-      </div>{" "}
+      </div>
       <div className="hero-container-fixed">
         <div className="hero-search">
           <form action="submit">
@@ -56,17 +95,34 @@ const Page: NextPage<Props> = async ({}) => {
         <div className="hero-card1">
           <h3>Hashtags</h3>
           <div className="hero-card1-items">
-            <Badge>#all</Badge>
-            <Badge>#ai</Badge>
-            <Badge>#tech</Badge>
-            <Badge>#gadgets</Badge>
-            <Badge>#ai</Badge>
-            <Badge>#tech</Badge>
-            <Badge>#gadgets</Badge>
-            <Badge>#Space</Badge>
-            <Badge>#Robots</Badge>
-            <Badge>#nano-tech</Badge>
-            <Badge>#Social-media</Badge>
+            <Badge
+              key="uncategorized"
+              onClick={() => handleTag("Uncategorized")}
+              className={
+                tag === "Uncategorized"
+                  ? "selected-hashtag"
+                  : "unselected-hashtag"
+              }
+            >
+              #Uncategorized
+            </Badge>
+            {Array.from(
+              new Set(
+                data.flatMap((item) =>
+                  item.hashtags.map((tag) => tag.toLowerCase())
+                )
+              )
+            ).map((hashtag, index) => (
+              <Badge
+                key={index}
+                onClick={() => handleTag(hashtag)}
+                className={
+                  tag === hashtag ? "selected-hashtag" : "unselected-hashtag"
+                }
+              >
+                #{hashtag}
+              </Badge>
+            ))}
           </div>
         </div>
       </div>
