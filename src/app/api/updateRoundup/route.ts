83d7e54f0a2335953f76bb3b7b175c9022cee62f
Shadowctrl <shadowctrl@outlook.com>
2024-08-13 22:21:46 +0530

    .

--- src/app/api/updateRoundup/route.ts
+++ src/app/api/updateRoundup/route.ts
@@ -1,9 +1,7 @@
 import clientPromise from "@/lib/db";
 import getS3Client from "@/lib/s3";
 import { PutObjectCommand, ObjectCannedACL } from "@aws-sdk/client-s3";
-export const config = {
-  maxDuration: 60,
-};
+export const maxDuration = 60;
 interface NewsContent {
   title: string[];
   headline: string[];
