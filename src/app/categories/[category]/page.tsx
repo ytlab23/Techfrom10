c2757d9f94c524c6a55e4a477e04929346000c50
Shadowctrl <shadowctrl@outlook.com>
2024-10-28 23:19:36 +0530

    "Added Switch component for unified view, updated date picker layout, and refactored news item rendering with two different views"

--- src/app/categories/[category]/page.tsx
+++ src/app/categories/[category]/page.tsx
@@ -7,6 +7,7 @@ import { FaExternalLinkAlt, FaEye, FaClock } from "react-icons/fa";
 import DatePickerComponent from "@/components/dataPicker/datePicker";
 import moment from "moment";
 import { useToast } from "@/components/ui/use-toast";
+import { Switch } from "@/components/ui/switch";
 import Footer from "@/components/footer/footer";
 import Loading from "../../loading";
 import { useRouter } from "next/navigation";
@@ -17,16 +18,17 @@ interface NewsItem {
   slugtitle: string;
   headlines: string;
   summary: string;
-  date: string; // Ensure this is a date string
+  date: string;
   img_url?: string;
   source?: string;
 }
 
 interface CategoryProps {
-  params: { category: string }; // Adjusted to match expected structure
+  params: { category: string };
 }
+
 interface DateRange {
-  from?: Date; // Make these optional to allow undefined
+  from?: Date;
   to?: Date;
 }
 
@@ -51,8 +53,13 @@ const CategoryPage = ({ params }: CategoryProps) => {
   } | null>(null);
   const { toast } = useToast();
   const [searchQuery, setSearchQuery] = useState<string>("");
+  const [unifiedView, setUnifiedView] = useState<boolean>(false);
 
   const hasReset = useRef(false);
+
+  const handleViewChange = (checked: boolean) => {
+    setUnifiedView(checked);
+  };
 
   const handleDateChange = (date: DateRange | null) => {
     if (date) {
@@ -79,7 +86,7 @@ const CategoryPage = ({ params }: CategoryProps) => {
       if (response.status === 200) {
         const categoryData: NewsItem[] = await response.json();
         setData(categoryData);
-        setFilteredData(categoryData); // Initialize filtered data
+        setFilteredData(categoryData);
       } else {
         router.push("/");
       }
@@ -114,7 +121,7 @@ const CategoryPage = ({ params }: CategoryProps) => {
       toast({
         title: "No results found for the selected Filter.",
       });
-      setFilteredData(data); // Reset to original data if no results
+      setFilteredData(data);
     } else {
       setFilteredData(newFilteredData);
     }
@@ -132,6 +139,128 @@ const CategoryPage = ({ params }: CategoryProps) => {
     event.preventDefault();
   };
 
+  const renderUnifiedView = (value: NewsItem) => (
+    <div className="hero-container unified-view" key={value.headline}>
+      <div className="hero-content-wrap">
+        <div className="category-right">
+          <ul>
+            <li>
+              <Link
+                href={`/article/${encodeURIComponent(
+                  value.headline.replaceAll(" ", "-")
+                )}`}
+                target="_blank"
+                rel="noreferrer nofollow noopener"
+                title="view article"
+              >
+                {value.headline}
+              </Link>
+              <div className="flex gap-2 items-center">
+                <Link
+                  href={`/article/${encodeURIComponent(
+                    value.headline.replaceAll(" ", "-")
+                  )}`}
+                  target="_blank"
+                  rel="noreferrer nofollow noopener"
+                  title="view article"
+                >
+                  <FaEye />
+                </Link>
+                {value.source && (
+                  <Link
+                    href={value.source}
+                    target="_blank"
+                    rel="noreferrer nofollow noopener"
+                    title="view full info"
+                  >
+                    <FaExternalLinkAlt fontSize={"12px"} />
+                  </Link>
+                )}
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <Link
+        href={
+          "/post/" + encodeURIComponent(value.slugtitle.replaceAll(" ", "-"))
+        }
+        target="_blank"
+        className="view-in-full"
+      >
+        Read Full Article
+      </Link>
+    </div>
+  );
+
+  const renderDefaultView = (value: NewsItem) => (
+    <div className="hero-container" key={value.headline}>
+      <div className="hero-container-head">
+        <span>
+          <FaClock className="text-base" />
+          {value.date}
+        </span>
+      </div>
+      <div className="hero-content-wrap">
+        {value.img_url && (
+          <Image
+            src={value.img_url}
+            alt={value.headline}
+            width={250}
+            height={300}
+          />
+        )}
+        <div className="category-right">
+          <ul>
+            <li>
+              <Link
+                href={`/article/${encodeURIComponent(
+                  value.headline.replaceAll(" ", "-")
+                )}`}
+                target="_blank"
+                rel="noreferrer nofollow noopener"
+                title="view article"
+              >
+                {value.headline}
+              </Link>
+              <div className="flex gap-2 items-center">
+                <Link
+                  href={`/article/${encodeURIComponent(
+                    value.headline.replaceAll(" ", "-")
+                  )}`}
+                  target="_blank"
+                  rel="noreferrer nofollow noopener"
+                  title="view article"
+                >
+                  <FaEye />
+                </Link>
+                {value.source && (
+                  <Link
+                    href={value.source}
+                    target="_blank"
+                    rel="noreferrer nofollow noopener"
+                    title="view full info"
+                  >
+                    <FaExternalLinkAlt fontSize={"12px"} />
+                  </Link>
+                )}
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <Link
+        href={
+          "/post/" + encodeURIComponent(value.slugtitle.replaceAll(" ", "-"))
+        }
+        target="_blank"
+        className="view-in-full"
+      >
+        Read Full Article
+      </Link>
+    </div>
+  );
+
   return (
     <div>
       {loading ? (
@@ -144,87 +273,23 @@ const CategoryPage = ({ params }: CategoryProps) => {
                 <Link href="/#">
                   <h3>Your Tech Round-Up!</h3>
                 </Link>
-                <DatePickerComponent onDateChange={handleDateChange} />
+                <div className="flex items-center gap-1">
+                  <h3>Unified View</h3>
+                  <Switch
+                    checked={unifiedView}
+                    onCheckedChange={handleViewChange}
+                  />
+                </div>{" "}
+                <div className="flex items-center gap-4">
+                  <DatePickerComponent onDateChange={handleDateChange} />
+                </div>
               </div>
               <div className="category-hero-container-wrap">
-                {filteredResults.map((value) => (
-                  <div>
-                    <div
-                      className="category-container-head"
-                      key={value.headline}
-                    >
-                      <div className="category-dir">
-                        <div className="category-date">
-                          <span>
-                            <FaClock className="text-base" />
-                            {value.date}
-                          </span>
-                        </div>
-                        <div className="category-image">
-                          {value.img_url && (
-                            <Image
-                              src={value.img_url}
-                              alt={value.headline}
-                              width={250}
-                              height={300}
-                            />
-                          )}
-                        </div>
-                        <div className="category-read-more">
-                          <Link
-                            href={
-                              "/post/" +
-                              encodeURIComponent(
-                                value.slugtitle.replaceAll(" ", "-")
-                              )
-                            }
-                            target="_blank"
-                          >
-                            Read Full Article
-                          </Link>
-                        </div>
-                      </div>
-                      <div className="category-right">
-                        <ul>
-                          <li>
-                            <Link
-                              href={`/article/${encodeURIComponent(
-                                value.headline.replaceAll(" ", "-")
-                              )}`}
-                              target="_blank"
-                              rel="noreferrer nofollow noopener"
-                              title="view article"
-                            >
-                              {value.headline}
-                            </Link>
-                          </li>
-                        </ul>
-                        <div className="flex gap-2 items-center">
-                          <Link
-                            href={`/article/${encodeURIComponent(
-                              value.headline.replaceAll(" ", "-")
-                            )}`}
-                            target="_blank"
-                            rel="noreferrer nofollow noopener"
-                            title="view article"
-                          >
-                            <FaEye />
-                          </Link>
-                          {value.source && (
-                            <Link
-                              href={`${value.source}`}
-                              target="_blank"
-                              rel="noreferrer nofollow noopener"
-                              title="view full info"
-                            >
-                              <FaExternalLinkAlt fontSize={"12px"} />
-                            </Link>
-                          )}
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                ))}
+                {filteredResults.map((value) =>
+                  unifiedView
+                    ? renderUnifiedView(value)
+                    : renderDefaultView(value)
+                )}
               </div>
             </div>
 
@@ -264,7 +329,7 @@ const CategoryPage = ({ params }: CategoryProps) => {
         </div>
       )}
       <div className="category-footer">
-      <Footer />
+        <Footer />
       </div>
     </div>
   );
