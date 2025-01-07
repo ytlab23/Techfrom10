9cfb5f1fed3cdec88bebe86f1d5f920a9b2d4d82
Shadowctrl <shadowctrl@outlook.com>
2025-01-07 19:40:42 +0530

    Category filter logic fix

--- src/app/api/fetchCategory/route.ts
+++ src/app/api/fetchCategory/route.ts
@@ -20,23 +20,32 @@ export const POST = async (req: Request) => {
     );
 
     if (res.rows.length > 0) {
-      // Prepare an array to hold all matching news items
       const responseData = res.rows.map((newsItem) => {
-        const index = newsItem.hashtags.indexOf(category);
-        return {
+        const indices = newsItem.hashtags.reduce(
+          (acc: number[], tag: string, idx: number) => {
+            if (tag === category) acc.push(idx);
+            return acc;
+          },
+          []
+        );
+
+        return indices.map((index: number) => ({
           title: newsItem.title,
           slugtitle: newsItem.slugtitle,
           headline: newsItem.headlines[index],
+          slugheadline: newsItem.slugheadlines[index],
           summary: newsItem.summaries[index],
           published: newsItem.published[index],
           img_url: newsItem.img_url,
           source: newsItem.sources[index],
           hashtags: newsItem.hashtags[index],
           date: newsItem.date,
-        };
+        }));
       });
 
-      return new Response(JSON.stringify(responseData));
+      const flattenedData = responseData.flat();
+
+      return new Response(JSON.stringify(flattenedData));
     } else {
       return new Response("", { status: 404 });
     }
